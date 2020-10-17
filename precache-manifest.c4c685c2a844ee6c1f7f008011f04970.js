343a632ee130ff388c4a6dc78dc6ace9b589bb0d
Lorenze Jay <Lorenzejay@gmail.com>
2020-10-17 12:12:15 -0700

    Updates

---
+++ precache-manifest.c4c685c2a844ee6c1f7f008011f04970.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967e212270d80eab74dc4d0baaad8f62",
+    "url": "/ljaph-Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "262715d5febaaf4d585d",
+    "url": "/ljaph-Covid-19-Tracker/static/css/main.74be57dc.chunk.css"
+  },
+  {
+    "revision": "1eed77ab0bf332d7b49e",
+    "url": "/ljaph-Covid-19-Tracker/static/js/2.502a4887.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/ljaph-Covid-19-Tracker/static/js/2.502a4887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262715d5febaaf4d585d",
+    "url": "/ljaph-Covid-19-Tracker/static/js/main.b774cfcb.chunk.js"
+  },
+  {
+    "revision": "33f3b4c5d8895cf31bbe",
+    "url": "/ljaph-Covid-19-Tracker/static/js/runtime-main.bb3753e3.js"
+  },
+  {
+    "revision": "a5c4222fc53ba1c7ef5dbe33903301d9",
+    "url": "/ljaph-Covid-19-Tracker/static/media/covid19Image.a5c4222f.jpg"
+  }
+]);
